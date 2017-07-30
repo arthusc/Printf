40380dce95866a8aab031bbbd8a8c880ad6f9561
Arthus CHAMBON <achambon@e3r9p10.42.fr>
2017-07-30 17:56:19 +0200

    Avancee du conv_c option -

--- Printf/conv_c.c
+++ Printf/conv_c.c
@@ -6,13 +6,66 @@
 /*   By: mbriffau <mbriffau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/27 15:10:00 by mbriffau          #+#    #+#             */
-/*   Updated: 2017/07/29 22:58:20 by mbriffau         ###   ########.fr       */
+/*   Updated: 2017/07/30 17:55:48 by achambon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "includes/ft_printf.h"
 
+char	*option_minus_c(va_list *format, int minimum_field_width)
+{
+	char *str;
+	int count;
+
+	count = 0;
+	str = malloc(sizeof(char) * ft_strlen((char*)format) + 1);
+	while (*format)
+	{
+		*str = (char)format;
+		format++;
+		str++;
+	}
+	while (count <= minimum_field_width)
+	{
+		*str = ' ';
+		str++;
+	}
+	*str = '\0';
+	return (str);
+}
+
+int		check_if_minimum_field_width(char *format)
+{
+	int minimum_field_width;
+	format = format;
+	minimum_field_width = 5;
+	return (minimum_field_width);
+}
 void	conv_c(t_printf *pf)
 {
-		ft_putchar(va_arg(*pf->ap, unsigned));
+	char *str;
+	
+//	va_arg(*pf->ap, unsigned);
+	if (ft_strchr("-0 ", *pf->format))
+			{
+				while (*pf->format)
+				{
+					if (*pf->format == '-')
+					{
+						pf->minimum_field_width = check_if_minimum_field_width(pf->format);
+							str = option_minus_c(pf->ap, pf->minimum_field_width);
+							ft_putstr(str);
+							return;
+					}
+					pf->format++;
+				}
+			}
+	ft_putchar(va_arg(*pf->ap, unsigned));
 }
+
+/* Can deal with option "-" if there is a minimal_width
+ * Can't deal with option "+" no matter what
+ * Can't deal with option "#"
+ * Can deal with option "0" and option "space"
+ */
+
