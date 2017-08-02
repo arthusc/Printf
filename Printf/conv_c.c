208070afdf5606ef7ad316966b2abd961adcd62a
Arthus CHAMBON <achambon@e2r10p6.42.fr>
2017-08-02 14:48:30 +0200

    Option 0 conv_c

--- Printf/conv_c.c
+++ Printf/conv_c.c
@@ -6,61 +6,59 @@
 /*   By: mbriffau <mbriffau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/27 15:10:00 by mbriffau          #+#    #+#             */
-/*   Updated: 2017/08/01 16:06:30 by mbriffau         ###   ########.fr       */
+/*   Updated: 2017/08/02 13:52:39 by achambon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "includes/ft_printf.h"
 
-// char	*option_minus_c(va_list *format, int minimum_field_width)
-// {
-// 	char *str;
-// 	int count;
-
-// 	count = 0;
-// 	str = malloc(sizeof(char) * ft_strlen((char*)format) + 1);
-// 	while (*format)
-// 	{
-// 		*str = (char)format;
-// 		format++;
-// 		str++;
-// 	}
-// 	while (count <= minimum_field_width)
-// 	{
-// 		*str = ' ';
-// 		str++;
-// 	}
-// 	*str = '\0';
-// 	return (str);
-// }
-
-// int		check_if_minimum_field_width(char *format)
-// {
-// 	int minimum_field_width;
-// 	format = format;
-// 	minimum_field_width = 5;
-// 	return (minimum_field_width);
-//}
-
-void	conv_c(t_printf *pf)
+/*char	*option_minus_c(va_list *format, int minimum_field_width)
 {
-// 	char *str;
-	
-// //	va_arg(*pf->ap, unsigned);
-// 	if (ft_strchr("-0 ", *pf->format))
-// 			{
-// 				while (*pf->format)
-// 				{
-// 					if (*pf->format == '-')
-// 					{
-// 						pf->minimum_field_width = check_if_minimum_field_width(pf->format);
-// 							str = option_minus_c(pf->ap, pf->minimum_field_width);
-// 							ft_putstr(str);
-// 							return;
-// 					}
-// 					pf->format++;
-// 				}
-// 			}
+	char *str;
+	int count;
+
+	count = 0;
+	str = malloc(sizeof(char) * ft_strlen((char*)format) + 1);
+	while (*format)
+	{
+		*str = (char)format;
+		format++;
+		str++;
+	}
+	while (count <= minimum_field_width)
+	{
+		*str = ' ';
+		str++;
+	}
+	*str = '\0';
+	return (str);
+}
+*/
+
+void	conv_c(t_printf *pf, t_conv *conv)
+{
+/*	char *str;
+
+	if (!(str = malloc(sizeof(char*))))
+		return;
+//	va_arg(pf->ap, unsigned);
+	while(!(ft_strchr("-0 ", pf->format[pf->i])))
+		pf->i++;
+	while (pf->format[pf->i])
+		{
+			if (pf->format[pf->i] == '-')
+			{
+				str = option_minus_c(&pf->ap, conv->min_width);
+				ft_putstr(str);
+				return;
+			}
+			pf->i++;
+		}*/
+	if (conv->flags->zero == 1)
+	{
+		while(conv->min_width-- > 1)
+			ft_putchar('0');
+	}
 	ft_putchar(va_arg(pf->ap, unsigned));
 }
 
