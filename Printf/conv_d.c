1ece971967383d72cfd8a1e65aa88175c7cfd640
Arthus CHAMBON <achambon@e2r10p6.42.fr>
2017-08-02 16:20:40 +0200

    conv_d pas fini

--- Printf/conv_d.c
+++ Printf/conv_d.c
@@ -6,7 +6,7 @@
 /*   By: mbriffau <mbriffau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/27 15:10:00 by mbriffau          #+#    #+#             */
-/*   Updated: 2017/08/01 15:39:15 by mbriffau         ###   ########.fr       */
+/*   Updated: 2017/08/02 16:09:21 by achambon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,26 @@
  *gere conv_i aussi
  */
 
-void	conv_d(t_printf *pf)
+void	conv_d(t_printf *pf, t_conv *conv)
 {
+	char *str;
+	int		len;
+	va_arg(pf->ap, int);
+
+	len = ft_strlen(str = ft_itoa((int)pf->ap));
+	printf("%d\n", len);
+	if (conv->flags->zero == 1 || conv->flags->minus == 0)
+		while (conv->min_width-- > 1)
+			ft_putchar('0');
+	if (conv->flags->space == 1)
+		while ( conv->min_width-- > 1)
+			ft_putchar(' ');
+	if (conv->flags->minus == 1)
+	{
+		ft_putnbr(va_arg(pf->ap, int));
+		while (conv->min_width-- > 1)
+			ft_putchar(' ');
+		return;
+	}
 	ft_putnbr(va_arg(pf->ap, int));
 }
