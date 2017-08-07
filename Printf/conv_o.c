4d103335da376f93d0c3792588cc756b27312844
Mikael BRIFFAUD <mbriffau@student.42.fr>
2017-08-07 10:20:33 +0200

    conv_o modif

--- Printf/conv_o.c
+++ Printf/conv_o.c
@@ -6,7 +6,7 @@
 /*   By: mbriffau <mbriffau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/05 19:35:35 by mbriffau          #+#    #+#             */
-/*   Updated: 2017/08/05 20:16:08 by mbriffau         ###   ########.fr       */
+/*   Updated: 2017/08/06 18:27:39 by mbriffau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,51 @@
 void		conv_o(t_printf *pf, t_conv *conv)
 {
 	unsigned int pointer_x;
-	//char *str;
+	char *str;
 	int i;
+	int len;
 
 	i = 0;
 	pointer_x = 0;
 	if (conv->modif == 'l')
 	{
 		pointer_x = ((unsigned long int)pointer_x);
-		pointer_x = va_arg(pf->ap, unsigned long int);
+		pointer_x = va_arg(pf->ap, uintmax_t);
+	}
+	else if (conv->modif == 'L')
+	{
+		pointer_x = ((unsigned long long int)pointer_x);
+		pointer_x = va_arg(pf->ap, uintmax_t);
+	}
+	else if (conv->modif == 'h')
+	{
+		pointer_x = ((uint16_t)pointer_x);
+		pointer_x = va_arg(pf->ap, uintmax_t);
+	}
+	else if (conv->modif == 'H')
+	{
+		pointer_x = ((uint8_t)pointer_x);
+		pointer_x = va_arg(pf->ap, uintmax_t);
+	}
+	else if (conv->modif == 'z')
+	{
+		pointer_x = ((size_t)pointer_x);
+		pointer_x = va_arg(pf->ap, uintmax_t);
+	}
+	else if (conv->modif == 'j')
+	{
+		pointer_x = ((uintmax_t)pointer_x);
+		pointer_x = va_arg(pf->ap, uintmax_t);
 	}
 	else
 		pointer_x = va_arg(pf->ap, unsigned int);
-	//str = ft_itoa_base((long long)pointer_x, 8);
-	ft_putstr(ft_itoa_base((long long)pointer_x, 8));
+
+	str = ft_itoa_base((long long)pointer_x, 8);
+	len = ft_strlen(str = ft_itoa(pointer_x));
+
+	if (conv->flags->zero == 1 && conv->flags->minus == 0)
+		while (conv->min_width-- > 0 + len)
+			ft_putchar('0');
+	ft_putstr(str);
 	return;
 }
--- Printf/main.c
+++ Printf/main.c
@@ -6,7 +6,7 @@
 /*   By: mbriffau <mbriffau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/31 23:29:04 by achambon          #+#    #+#             */
-/*   Updated: 2017/08/05 20:16:58 by mbriffau         ###   ########.fr       */
+/*   Updated: 2017/08/07 10:12:50 by mbriffau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,29 @@
 
 int		main(int ac, char **av)
 {
+	size_t a = 123456;
+	uintmax_t max = 789456123;
+	unsigned char uchar = 65; 
 	if (ac)
 
-	ft_printf("abcd%s\n", " 1");
-	ft_printf("efg%-15s\n", " 1");
-	ft_printf("->%-10c\n", 'a');
-	ft_printf("->%s\n", "test_s");
-	ft_printf("->%.2d\n", 42);
-	ft_printf("->%-10s\n", "Salut");
-	ft_printf("->%ls\n", L"ひらがな");
-	ft_printf("->%lc\n", L'ひ');
-	ft_printf("%O\n", 30);
-	printf("%O\n", 30);
+	// ft_printf("abcd%s\n", " 1");
+	// ft_printf("efg%-15s\n", " 1");
+	// ft_printf("->%-10c\n", 'a');
+	// ft_printf("->%s\n", "test_s");
+	// ft_printf("->%.2d\n", 42);
+	// ft_printf("->%-10s\n", "Salut");
+	// ft_printf("->%ls\n", L"ひらがな");
+	// ft_printf("->%llc\n", L'ひ');
+	ft_printf("m :%O\n", 30);
+	printf("t :%O\n", 30);
+	ft_printf("m :%zo\n", a);
+	printf("t :%50zo\n", a);
+	ft_printf("m :%jo\n", max);
+	printf("t :%jo\n", max);
+	ft_printf("m :%010o\n", 60);
+	printf("t :%010o\n", 60);
+	ft_printf("m :%hho\n", uchar);
+	printf("t :%hho\n", uchar);
 	return (0);
 }
 
