10dcf4cc23810f425cc4a38aee98af19a9a26776
Arthus CHAMBON <achambon@e1r6p9.42.fr>
2017-08-10 20:47:20 +0200

    debut nouveau conv_d et main de test

--- Printf/conv_d.c
+++ Printf/conv_d.c
@@ -6,7 +6,7 @@
 /*   By: mbriffau <mbriffau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/27 15:10:00 by mbriffau          #+#    #+#             */
-/*   Updated: 2017/08/08 00:08:10 by mbriffau         ###   ########.fr       */
+/*   Updated: 2017/08/10 20:29:10 by achambon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,15 +64,15 @@ void	conv_d_h(t_printf *pf, t_conv *conv)
 		ft_error("error_conv_d\n");
 	len = ft_strlen(str = ft_itoa(apint));
 	if (conv->flags->zero == 1 && conv->flags->minus == 0)
-		while (conv->min_width-- > 0 + len)
+		while (conv->min_width-- - len >= 0)
 			ft_putchar('0');
 	if (conv->flags->space == 1)
-		while ( conv->min_width-- > 0 + len)
+		while ( conv->min_width-- -len >= 0)
 			ft_putchar(' ');
 	if (conv->flags->minus == 1)
 	{
 		ft_putnbr(apint);
-		while (conv->min_width-- > 0 + len)
+		while (conv->min_width-- - len >= 0)
 			ft_putchar(' ');
 		return;
 	}
@@ -95,11 +95,33 @@ void	conv_d(t_printf *pf, t_conv *conv)
 	if(!(apint = va_arg(pf->ap, int)))
 		ft_error("error_conv_d\n");
 	len = ft_strlen(str = ft_itoa(apint));
+	
+	if (!conv->min_width && !conv->precision)
+
+
+	else if (conv->min_width && !conv->precision)
+
+	else if (!conv->min_width && conv->precision)
+
+	else if (conv->min_width && conv->precision)
+
+
+	return;
+/*
+
 	if (conv->flags->zero && !conv->flags->minus)
-		conv->min_width = option(conv->min_width - len, '0', 0, 0);
+		conv->min_width = option(conv->min_width - len, '0', conv, 0);
+
 	if (conv->flags->space == 1)
+	{
+		if(conv->precision && !conv->min_width)
+		{
+			ft_putchar(' ');
+			
+		}
 		while ( conv->min_width-- > 0 + len)
 			ft_putchar(' ');
+	}
 	if (conv->flags->minus == 1)
 	{
 		if(conv->precision_set > 0)
@@ -113,12 +135,33 @@ void	conv_d(t_printf *pf, t_conv *conv)
 			ft_putchar(' ');
 		return;
 	}
-	else
-		while (conv->min_width-- > 0 + conv->precision)
+	
+	if (conv->min_width && !conv->precision)
+	{
+		option(conv->min_width - len, ' ', conv, str);
+		return;
+	}
+
+	if(conv->min_width && conv->precision)
+	{
+		if (conv->flags->plus)
+			conv->min_width--;
+		while (conv->min_width-- - conv->precision)
+			ft_putchar(' ');
+		if (conv->flags->plus)
+			ft_putchar('+');
+		while(conv->precision-- - len)
+			ft_putchar('0');
+		ft_putstr(str);
+		return;
+	}
+
+
+	while (conv->min_width-- > 0 + conv->precision)
 			ft_putchar(' ');
 	if(conv->precision_set > 0)
 		while ((conv->precision-- - len) >= 1)
 			ft_putchar('0');
 
-	ft_putnbr(apint);
+	ft_putnbr(apint);*/
 }
--- Printf/main.c
+++ Printf/main.c
@@ -6,7 +6,7 @@
 /*   By: mbriffau <mbriffau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/31 23:29:04 by achambon          #+#    #+#             */
-/*   Updated: 2017/08/10 14:35:32 by achambon         ###   ########.fr       */
+/*   Updated: 2017/08/10 20:35:38 by achambon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,15 @@ int		main(int ac, char **av)
 {
 	size_t a = 123456;
 	 uintmax_t max = 789456123;
-	 unsigned char uchar = 65; 
+	 unsigned char uchar = 65;
 	if (ac)
 	 {
 	 	 ac = ac;
 	 }
-	int c = 10;
+	int c = 14574;
 	int *d = &c;
-//	ft_printf("abcd%s\n", " 1");
+/*
+	//	ft_printf("abcd%s\n", " 1");
 //	ft_printf("efg%-15s\n", " 1");
 //	ft_printf("->%-10c\n", 'a');
 //	ft_printf("->%s\n", "test_s");
@@ -31,28 +32,31 @@ int		main(int ac, char **av)
 //	ft_printf("->%-10s\n", "Salut");
 //	ft_printf("->%06S\n", L"ひらがな");
 //	ft_printf("->%C\n", L'ひ');
+
 //	ft_printf("m :%lo\n", 30);
 //	ft_printf("m :% 15zo\n", a);
 //	ft_printf("m :%jo\n", max);
 //	ft_printf("m :%010o\n", 60);
 //	ft_printf("m :%hho\n", uchar);
+
 //	 ft_printf("m :%p\n", d);
 //	 printf("t :%p\n", d);
 //	 ft_printf("m :%20p\n", d);
 //	 printf("t :%20p\n", d);
 //	 ft_printf("m :%10p\n", d);
 //	 printf("t :%10p\n", d);
-//	 ft_printf("m :%010p\n", d);
-//	 printf("t :%010p\n", d);
- 	 ft_printf("m :%.5p\n", d);
+	
+	ft_printf("m :%010p\n", d);
+	 printf("t :%010p\n", d);
+	 ft_printf("m :%.5p\n", d);
 	 printf("t :%.5p\n", d);
- 	 ft_printf("m :%.25p\n", d);
+	 ft_printf("m :%.25p\n", d);
 	 printf("t :%.25p\n", d);
- 	 ft_printf("m :%-.5p\n", d);
+	 ft_printf("m :%-.5p\n", d);
 	 printf("t :%-.5p\n", d);
- 	 ft_printf("m :%-.25p\n", d);
+	 ft_printf("m :%-.25p\n", d);
 	 printf("t :%-.25p\n", d);
- 	 ft_printf("m :%0.5p\n", d);
+	 ft_printf("m :%0.5p\n", d);
 	 printf("t :%0.5p\n", d);
 	 ft_printf("m :%-0.25p\n", d);
 	 printf("t :%-0.25p\n", d);
@@ -60,17 +64,106 @@ int		main(int ac, char **av)
 	 printf("t :%- 25p\n", d);
 	 ft_printf("m :%10.20p\n", d);
 	 printf("t :%10.20p\n", d);
- 	 ft_printf("m :%20.10p\n", d);
+	 ft_printf("m :%20.10p\n", d);
 	 printf("t :%20.10p\n", d);
 	 ft_printf("m :%-10.20p\n", d);
 	 printf("t :%-10.20p\n", d);
- 	 ft_printf("m :%+20.30p\n", d);
+	 ft_printf("m :%+20.30p\n", d);
 	 printf("t :%+20.30p\n", d);
-	 ft_printf("m :%-10.5p\n", d);
-	 printf("t :%-10.5p\n", d);
+	 ft_printf("m :%50.100p\n", d);
+	 printf("t :%50.100p\n", d);
 	 ft_printf("m :%-010.5p\n", d);
 	 printf("t :%-010.5p\n", d);
 
+	printf("t :%10u\n", c);
+	ft_printf("m :%10u\n", c);
+	printf("t :%.10u\n", c);
+	ft_printf("m :%.10u\n", c);
+	printf("t :%10.5u\n", c);
+	ft_printf("m :%10.5u\n", c);
+	printf("t :%5.10u\n", c);
+	ft_printf("m :%5.10u\n", c);
+	printf("t :%-10u\n", c);
+	ft_printf("m :%-10u\n", c);
+	printf("t :%-.10u\n", c);
+	ft_printf("m :%-.10u\n", c);
+	printf("t :%-10.5u\n", c);
+	ft_printf("m :%-10.5u\n", c);
+	printf("t :%-5.10u\n", c);
+	ft_printf("m :%-5.10u\n", c);/
+
+	printf("t :%10o\n", c);
+	ft_printf("m :%10o\n", c);
+	printf("t :%.10o\n", c);
+	ft_printf("m :%.10o\n", c);
+	printf("t :%10.5o\n", c);
+	ft_printf("m :%10.5o\n", c);
+	printf("t :%5.10o\n", c);
+	ft_printf("m :%5.10o\n", c);
+	printf("t :%-10o\n", c);
+	ft_printf("m :%-10o\n", c);
+	printf("t :%-.10o\n", c);
+	ft_printf("m :%-.10o\n", c);
+	printf("t :%-10.5o\n", c);
+	ft_printf("m :%-10.5o\n", c);
+	printf("t :%-5.10o\n", c);
+	ft_printf("m :%-5.10o\n", c);
+
+	printf("t :%10x\n", c);
+	ft_printf("m :%10x\n", c);
+	printf("t :%.10x\n", c);
+	ft_printf("m :%.10x\n", c);
+	printf("t :%10.5x\n", c);
+	ft_printf("m :%10.5x\n", c);
+	printf("t :%5.10x\n", c);
+	ft_printf("m :%5.10x\n", c);
+	printf("t :%-10x\n", c);
+	ft_printf("m :%-10x\n", c);
+	printf("t :%-.10x\n", c);
+	ft_printf("m :%-.10x\n", c);
+	printf("t :%-10.5x\n", c);
+	ft_printf("m :%-10.5x\n", c);
+	printf("t :%-5.10x\n", c);
+	ft_printf("m :%-5.10x\n", c);
+*/
+	ft_printf("m :%10.5d\n", 784);
+	printf("t :%10.5d\n", 784);
+	ft_printf("m :% 10.5d\n", 784);
+	printf("t :% 10.5d\n", 784);
+	ft_printf("m :%+10.5d\n", 784);
+	printf("t :%+10.5d\n", 784);
+	ft_printf("m :%-10.5d\n", 784);
+	printf("t :%-10.5d\n", 784);
+	ft_printf("m :%+-10.5d\n", 784);
+	printf("t :%+-10.5d\n", 784);
+	ft_printf("m :%-010.5d\n", 784);
+	printf("t :%-010.5d\n", 784);
+	ft_printf("m :%+010.5d\n", 784);
+	printf("t :%+010.5d\n", 784);
+	ft_printf("m :%-+010.5d\n", 784);
+	printf("t :%-+010.5d\n", 784);
+	ft_printf("m :% 010.5d\n", 784);
+	printf("t :% 010.5d\n", 784);
+
+
+	ft_printf("t :% .5d\n", 784);
+	printf("t :% .5d\n", 784);
+	ft_printf("t :% .5d\n", 784);
+	printf("t :% .5d\n", 784);
+	ft_printf("t :% .5d\n", 784);
+	printf("t :% .5d\n", 784);
+
+	ft_printf("t :% 5d\n", 784);
+	printf("t :% 5d\n", 784);
+	ft_printf("t :% 5d\n", 784);
+	printf("t :% 5d\n", 784);
+	ft_printf("t :% 5d\n", 784);
+	printf("t :% 5d\n", 784);
+//	printf("% s\n", "14 is a positive number");
+//	printf("% u\n", 747);
+//	printf("% x\n", 784);
+//	printf("% o\n", 784);
+//	printf("% p\n", 784);
 	return (0);
 }
 
