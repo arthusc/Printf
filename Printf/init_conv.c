27abc88dffd9a190737755755b1878a6b9f03920
Mikael BRIFFAUD <mbriffau@student.42.fr>
2017-08-11 13:26:26 +0200

    update init_conv.c

--- Printf/init_conv.c
+++ Printf/init_conv.c
@@ -6,20 +6,11 @@
 /*   By: mbriffau <mbriffau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/01 23:38:21 by mbriffau          #+#    #+#             */
-/*   Updated: 2017/08/02 15:57:11 by mbriffau         ###   ########.fr       */
+/*   Updated: 2017/08/11 13:25:11 by mbriffau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "includes/ft_printf.h"
-
-static t_flags		*init_flags(void)
-{
-	t_flags		*newflags;
-
-	newflags = malloc(sizeof(t_flags));
-	ft_bzero(newflags, sizeof(t_flags));
-	return (newflags);
-}
 
 t_conv				*init_conv(void)
 {
@@ -28,7 +19,6 @@ t_conv				*init_conv(void)
 	if (!(conv = malloc(sizeof(t_conv))))
 		ft_error("error_malloc_init_conv");
 	ft_bzero(conv, sizeof(t_conv));
-	conv->flags = init_flags();
 	conv->modif = '0';
 	return (conv);
 }
