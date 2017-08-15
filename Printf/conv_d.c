67b73b8c37f1c07f928e8f1755c4879bd302cab4
Arthus CHAMBON <achambon@e1r4p10.42.fr>
2017-08-15 18:30:28 +0200

    conv_d need compressor, main

--- Printf/conv_d.c
+++ Printf/conv_d.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   conv_d.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mbriffau <mbriffau@student.42.fr>          +#+  +:+       +#+        */
+/*   By: achambon <achambon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/27 15:10:00 by mbriffau          #+#    #+#             */
-/*   Updated: 2017/08/14 20:50:04 by achambon         ###   ########.fr       */
+/*   Updated: 2017/08/15 18:28:20 by achambon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,49 @@
  NE GERE PAS LA PRECISON => x.xx = FLOAT
  */
 
+int	option_d(int n, char c, t_conv *conv, char *s)
+{
+	int		i;
+	char	tab[n + 1];
+
+	i = 0;
+	tab[n] = '\0';
+	if (!n)
+		return(0);
+	if (!(conv->before == 3) && conv->before && s)
+	{
+		if(conv->ox == 1)
+			ft_putstr("0x");
+		ft_putstr(s);
+	}
+	if (conv->precision_set == 2)
+	{
+		ft_putstr("0x");
+		conv->ox = 0;
+	}
+	if(!(conv->flag & MINUS) || (conv->flag & MINUS && conv->min_width > conv->precision) || conv->before == 3)
+	{
+		while (i < n)
+			tab[i++] = c;
+		write(1, tab, n);
+	}
+	if((conv->flag & PLUS) && !(conv->flag & ZERO) && !(conv->flag & MINUS) && (conv->flag & (TYPE_D + TYPE_U + TYPE_O)) && !(conv->before == 3) && !conv->flag & MODIFIER_HH)
+		ft_putchar('+');
+	if ((conv->before == 0 && s) || conv->before == 3)
+	{
+		if(conv->ox == 1)
+			ft_putstr("0x");
+		ft_putstr(s);
+	}
+	if((conv->flag & MINUS && !(conv->min_width > conv->precision)) && !(conv->before == 3))
+	{
+		while (i < n)
+			tab[i++] = c;
+		write(1, tab, n);
+	}
+	
+	return (n - i);
+}
 
 int	conv_d_minus(t_conv *conv, int len, char *str)
 {
@@ -31,12 +74,12 @@ int	conv_d_minus(t_conv *conv, int len, char *str)
 		}
 			else if(conv->min_width > len)
 		{
-			if(conv->flag & SPACE && !(conv->flag & PLUS))
+			if(conv->flag & SPACE && !(conv->flag & PLUS) && !(conv->flag & MODIFIER_HH))
 			{
 				ft_putchar(' ');
 				conv->min_width--;
 			}
-			if(conv->flag & PLUS && !(conv->flag & ZERO) && !(conv->flag & SPACE))
+			if(conv->flag & PLUS && !(conv->flag & ZERO) && !(conv->flag & SPACE) && !(conv->flag & MODIFIER_HH))
 			{
 				conv->min_width--;
 				conv->before = 1;
@@ -47,7 +90,7 @@ int	conv_d_minus(t_conv *conv, int len, char *str)
 			{
 				conv->min_width--;
 			}
-			else if(conv->flag & PLUS && conv->flag & ZERO)
+			else if(conv->flag & PLUS && conv->flag & ZERO && !(conv->flag & MODIFIER_HH))
 			{
 				conv->before = 1;
 				ft_putchar('+');
@@ -71,10 +114,15 @@ int	conv_d_minus(t_conv *conv, int len, char *str)
 	if (!conv->min_width && conv->precision)
 	{
 		conv->before = 3;
-		if (conv->flag & SPACE && !(conv->flag & PLUS))
+		if (conv->flag & SPACE && !(conv->flag & PLUS) && !(conv->flag & MODIFIER_HH))
 			ft_putchar(' ');
-		if (conv->flag & PLUS)
+		if (conv->flag & PLUS && !(conv->flag & MODIFIER_HH))
 			ft_putchar('+');
+		if(str[0] == '-')
+		{
+			str[0] = '0';
+			ft_putchar('-');
+		}
 		return(option(conv->precision - len, '0', conv, str));
 	}
 //LARGEUR MINIMALE ET PRECISION PRESENTES
@@ -83,15 +131,21 @@ int	conv_d_minus(t_conv *conv, int len, char *str)
 		if(conv->min_width > conv->precision)
 		{
 			conv->before = 0;
-			if(conv->flag & SPACE && !(conv->flag & PLUS))
+			if(conv->flag & SPACE && !(conv->flag & PLUS) && !(conv->flag & MODIFIER_HH))
 			{
 				conv->min_width--;
 				ft_putchar(' ');
 			}
-				if(conv->flag & PLUS)
+				if(conv->flag & PLUS && !(conv->flag & MODIFIER_HH))
 			{
 				conv->min_width--;
 				ft_putchar('+');
+			}
+			if (str[0] == '-')
+			{
+				str[0] = '0';
+				ft_putchar('-');
+				conv->min_width--;
 			}
 			option(conv->precision - len, '0', conv, str);
 			while((conv->min_width-- - conv->precision))
@@ -100,12 +154,17 @@ int	conv_d_minus(t_conv *conv, int len, char *str)
 		}
 		if (conv->min_width <= conv->precision)
 		{
-			if(conv->flag & PLUS)
+			if(str[0] == '-')
+			{
+				ft_putchar('-');
+				str[0] = '0';
+			}
+			if(conv->flag & PLUS && !(conv->flag & MODIFIER_HH))
 			{
 				conv->min_width--;
 				ft_putchar('+');
 			}
-			if(conv->flag & SPACE && !(conv->flag & PLUS))
+			if(conv->flag & SPACE && !(conv->flag & PLUS + MODIFIER_HH))
 			{
 				conv->min_width--;
 				ft_putchar(' ');
@@ -117,13 +176,145 @@ int	conv_d_minus(t_conv *conv, int len, char *str)
 	}
 	ft_putstr(str);
 	return(0);
-
 }
+int		conv_d_hh_nominus(t_conv *conv, char *str, int len)
+{
+
+//JUSTE LARGEUR MINIMALE
+	if (conv->min_width && !conv->precision)
+	{
+		if(conv->min_width < len)
+		{
+			if(conv->flag & PLUS && str[0] != '-')
+			{
+				ft_putchar('+');
+				ft_putstr(str);
+				return(0);
+			}
+			if(conv->flag & SPACE && !(conv->flag & ZERO))
+			{
+				ft_putchar(' ');
+				ft_putstr(str);
+				return(0);
+			}
+			ft_putstr(str);
+			return(0);
+		}
+		if(conv->min_width >= len)
+		{
+			//if(conv->flag & SPACE && !(conv->flag & PLUS))
+			//{
+			//	ft_putchar(' ');
+				//conv->min_width--;
+			//}
+			if(conv->flag & PLUS && !(conv->flag & ZERO + MODIFIER_HH))
+				conv->min_width--;
+			else if(str[0] != '-' && conv->flag & PLUS && conv->flag & ZERO && !(conv->flag & MODIFIER_HH))
+			{
+				ft_putchar('+');
+				return(option(conv->min_width - 1 - len, '0', conv, str));
+			}
+			else if(conv->flag & ZERO)
+			{
+				if(str[0] == '-')
+				{
+					str[0] = '0';
+					ft_putchar('-');
+					ft_putstr(str);
+					return (0);
+				}
+				ft_putchar('G');
+				return(option(conv->min_width - len, '0', conv, str));
+			}
+			// 	if(conv->flag & MODIFIER_HH /*&& !(conv->flag & ZERO + PLUS + SPACE)*/)
+			// {
+			// 	while(conv->min_width-- - len)
+			// 		ft_putchar(' ');
+			// 	ft_putstr(str);
+			// 	return(0);
+			// }
+			return(option(conv->min_width - len, ' ', conv, str));
+		}
+	}
+//JUSTE PRECISION
+	if (!conv->min_width && conv->precision)
+	{
+		conv->before = 3;
+		if (conv->flag & SPACE && !(conv->flag & PLUS) && !(conv->flag & MODIFIER_HH))
+			ft_putchar(' ');
+		if (conv->flag & PLUS && !(conv->flag & MODIFIER_HH))
+			ft_putchar('+');
+		if(str[0] == '-')
+		{
+			ft_putchar('-');
+			str[0] = '0';
+		}
+		return(option(conv->precision - len, '0', conv, str));
+			return(0);
+	}
+//LARGEUR MINIMALE ET PRECISION PRESENTES
+	if (conv->min_width && conv->precision)
+	{
+		if(conv->min_width > conv->precision)
+		{
+			conv->before = 0;
+			if(conv->flag & ZERO && !(conv->flag & PLUS))
+				conv->min_width--;
+			if(conv->flag & SPACE)
+			{
+				conv->min_width--;
+				if(conv->flag & PLUS + ZERO)
+					ft_putchar(' ');
+			}
+			if(conv->flag & PLUS)
+				conv->min_width--;
+			while((conv->min_width-- - conv->precision))
+				ft_putchar(' ');
+			if(conv->flag & PLUS && str[0] != '-')
+			{
+				conv->before = 3;
+				ft_putchar('+');
+			}
+			if(str[0] == '-')
+			{
+				str[0] = '0';
+				ft_putchar('-');
+			}
+			option(conv->precision - len, '0', conv, str);
+			return(0);
+		}
+		if (conv->min_width <= conv->precision)
+		{
+			if(str[0] == '-')
+			{
+				str[0] = '0';
+				ft_putchar('-');
+			}
+		//	if(conv->flag & PLUS)
+		//	{
+		//		conv->min_width--;
+		//		ft_putchar('+');
+		//	}
+		//	if(conv->flag & SPACE && !(conv->flag & PLUS))
+		//	{
+		//		conv->min_width--;
+		//		ft_putchar(' ');
+		//	}
+			conv->before = 3;
+			option(conv->precision - len, '0', conv, str);
+			return(0);
+		}
+	}
+		ft_putstr(str);
+	return(0);
+}
+
 int		conv_d(t_printf *pf, t_conv *conv)
 {
 	int		len;
 	int		apint;
 	char	*str;
+	int		flag_hh;
 
 	conv->flag & MODIFIER_H ? apint = va_arg(pf->ap, short) : apint == 1;
 	conv->flag & MODIFIER_HH ? apint = va_arg(pf->ap, char) : apint == 1;
@@ -146,12 +337,30 @@ int		conv_d(t_printf *pf, t_conv *conv)
 		ft_putstr(str);
 		return(0);
 	}
-int a = conv->min_width;
+	int width_temp = conv->min_width;
+	if (conv->flag & MODIFIER_HH && !(conv->flag & MINUS))
+	{
+		conv_d_hh_nominus(conv, str, len);
+		return(0);
+	}
 	if (conv->flag & MINUS)
 	{
 		(conv_d_minus(conv, len, str));
-		if (a > len)
+		if (conv->min_width > len || width_temp > len)
 		{
+			if (str[0] == '-')
+			{
+//				ft_putchar('G')
+				width_temp--;
+				if (conv->flag & PLUS && !(conv->flag & SPACE + ZERO))
+					width_temp++;
+			}
+				if (conv->flag & MODIFIER_HH && !(conv->flag & SPACE + ZERO) && conv->flag & PLUS && !(conv->precision))
+				{
+					ft_putstr(str);
+					while(width_temp-- - len)
+						ft_putchar(' ');
+				}
 			return (0);
 		}
 		if(conv->min_width < len && !(conv->flag & SPACE) && !(conv->flag & PLUS) && !conv->precision)
@@ -160,23 +369,31 @@ int a = conv->min_width;
 			return(0);
 		}
 	}
-//	if(str[0] == '-')
-//	{
-//		conv->min_width--;
-//	}
+	if(str[0] == '-' && conv->flag & MINUS)
+	{
+		flag_hh = 1;
+		conv->min_width--;
+	}
 //JUSTE LARGEUR MINIMALE
 	if (conv->min_width && !conv->precision)
 	{
 		if(conv->min_width < len)
 		{
-			if(conv->flag & PLUS)
+			if(conv->flag & PLUS && width_temp < len)
 			{
 				ft_putchar('+');
 				ft_putstr(str);
 				return(0);
 			}
-			if(conv->flag & SPACE)
+			if(conv->flag & SPACE || ((conv->flag & MODIFIER_HH) && !(conv->flag & ZERO)))
 			{
+//				if(conv->flag & MODIFIER_HH)
+//				{
+//					ft_putchar('Z');
+//					ft_putstr(str);
+//					ft_putchar(' ');
+//					return(0);
+//				}
 				ft_putchar(' ');
 				ft_putstr(str);
 				return(0);
@@ -184,22 +401,38 @@ int a = conv->min_width;
 			ft_putstr(str);
 			return(0);
 		}
-		else if(conv->min_width >= len)
+		if(conv->min_width >= len || width_temp >= len)
 		{
 			if(conv->flag & SPACE && !(conv->flag & PLUS))
 			{
 				ft_putchar(' ');
 				conv->min_width--;
 			}
-			if(conv->flag & PLUS && !(conv->flag & ZERO))
+			if(conv->flag & PLUS && !(conv->flag & ZERO + MODIFIER_HH))
 				conv->min_width--;
-			else if(conv->flag & PLUS && conv->flag & ZERO)
+			else if(conv->flag & PLUS && conv->flag & ZERO && !(conv->flag & MODIFIER_HH))
 			{
 				ft_putchar('+');
 				return(option(conv->min_width - 1 - len, '0', conv, str));
 			}
 			else if(conv->flag & ZERO)
+			{
+				if(str[0] == '-')
+				{
+					str[0] = '0';
+					ft_putchar('-');
+					ft_putstr(str);
+					return (0);
+				}
 				return(option(conv->min_width - len, '0', conv, str));
+			}
+				if(conv->flag & MODIFIER_HH /*&& !(conv->flag & ZERO + PLUS + SPACE)*/)
+			{
+				while(conv->min_width-- - len)
+					ft_putchar(' ');
+				ft_putstr(str);
+				return(0);
+			}
 			return(option(conv->min_width - len, ' ', conv, str));
 		}
 	}
@@ -207,15 +440,20 @@ int a = conv->min_width;
 	if (!conv->min_width && conv->precision)
 	{
 		conv->before = 3;
-		if (conv->flag & SPACE && !(conv->flag & PLUS))
+		if (conv->flag & SPACE && !(conv->flag & PLUS) && !(conv->flag & MODIFIER_HH))
 			ft_putchar(' ');
-		if (conv->flag & PLUS)
+		if (conv->flag & PLUS && !(conv->flag & MODIFIER_HH))
 			ft_putchar('+');
-		return(option(conv->precision - len, '0', conv, str));
+		if(!(conv->flag & MODIFIER_HH))
+			return(option(conv->precision - len, '0', conv, str));
+		else
+			return(0);
 	}
 //LARGEUR MINIMALE ET PRECISION PRESENTES
 	if (conv->min_width && conv->precision)
 	{
+		if(conv->flag & MODIFIER_HH)
+			return(0);
 		if(conv->min_width > conv->precision)
 		{
 			conv->before = 0;
@@ -238,11 +476,8 @@ int a = conv->min_width;
 		}
 		if (conv->min_width <= conv->precision)
 		{
-			if(conv->flag & PLUS)
-			{
-				conv->min_width--;
-				ft_putchar('+');
-			}
+			conv->flag & PLUS ? ft_putchar('+') : 0;
+			conv->flag & PLUS ? conv->min_width-- : 0; 
 			if(conv->flag & SPACE && !(conv->flag & PLUS))
 			{
 				conv->min_width--;
@@ -253,6 +488,6 @@ int a = conv->min_width;
 			return(0);
 		}
 	}
-	ft_putstr(str);
+		ft_putstr(str);
 	return(0);
 }
--- Printf/main.c
+++ Printf/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mbriffau <mbriffau@student.42.fr>          +#+  +:+       +#+        */
+/*   By: achambon <achambon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/14 20:13:45 by achambon          #+#    #+#             */
-/*   Updated: 2017/08/15 02:33:06 by mbriffau         ###   ########.fr       */
+/*   Updated: 2017/08/15 18:29:43 by achambon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,22 +19,22 @@ int		main(int ac, char **av)
 	if (ac)
 	setlocale(LC_ALL, "");
 //-------------TEST_S-----------------
-	ft_printf("->%011s\n", "abc");
-	printf("+>%011s\n", "abc");
-	ft_printf("->%011s\n", "abc");
-	printf("+>%011s\n", "abc");
-	ft_printf("->%011ls\n", L"aらが");
-	printf("+>%011ls\n", L"らが");
-	ft_printf("->%011ls\n", L"ひらが");
-	printf("+>%011ls\n", L"ひらが");
-	ft_printf("->%-11S|\n", L"ひらが");
-	printf("+>%-11S|\n", L"ひらが");
-	ft_printf("->%+11S\n", L"ひらが");
-	printf("+>%+11S\n", L"ひらが");
-	ft_printf("->%011S\n", L"ひらが");
-	printf("+>%011S\n", L"ひらが");
-	ft_printf("->% 11S\n", L"ひらが");
-	printf("+>% 11S\n", L"ひらが");
+	// ft_printf("->%011s\n", "abc");
+	// printf("+>%011s\n", "abc");
+	// ft_printf("->%011s\n", "abc");
+	// printf("+>%011s\n", "abc");
+	// ft_printf("->%011ls\n", L"aらが");
+	// printf("+>%011ls\n", L"らが");
+	// ft_printf("->%011ls\n", L"ひらが");
+	// printf("+>%011ls\n", L"ひらが");
+	// ft_printf("->%-11S|\n", L"ひらが");
+	// printf("+>%-11S|\n", L"ひらが");
+	// ft_printf("->%+11S\n", L"ひらが");
+	// printf("+>%+11S\n", L"ひらが");
+	// ft_printf("->%011S\n", L"ひらが");
+	// printf("+>%011S\n", L"ひらが");
+	// ft_printf("->% 11S\n", L"ひらが");
+	// printf("+>% 11S\n", L"ひらが");
 
 	// printf("+>%011s\n", "abc");
 	// printf("+>%011s\n", "abc");
@@ -111,7 +111,7 @@ int		main(int ac, char **av)
 
 // //TEST AVEC %hhd MINUS
 
-// //_____convminwidth < len 
+// _____convminwidth < len 
 // 	ft_printf("m :%-hhd\n", 123);
 // 	printf("t :%-hhd\n", 123);
 // 	ft_printf("m :%-1hhd\n", 124);
@@ -128,176 +128,176 @@ int		main(int ac, char **av)
 // 	printf("t :%-+01hhd\n", 129);
 // 	ft_printf("m :%-+ 01hhd\n", 129);
 // 	printf("t :%-+ 01hhd\n", 129);
-//______convminwidth > len
-//	ft_printf("m :%-5hhd\n", 130);
-//	printf("t :%-5hhd\n", 130);
-//	ft_printf("m :%-05hhd\n", 131);
-//	printf("t :%-05hhd\n", 131);
-// 	ft_printf("m :%- 5hhd\n", 132);
-// 	printf("t :%- 5hhd\n", 132);
-// 	ft_printf("m :%-+5hhd\n", 133);
-// 	printf("t :%-+5hhd\n", 133);
-// 	ft_printf("m :%- 05hhd\n", 134);
-// 	printf("t :%- 05hhd\n", 134);
-// 	ft_printf("m :%-+05hhd\n", 135);
-// 	printf("t :%-+05hhd\n", 135);
-// 	ft_printf("m :%-+ 05hhd\n", 135);
-// 	printf("t :%-+ 05hhd\n", 135);
-// //________convprecision
-// 	ft_printf("m :%-.5hhd\n", 136);
-// 	printf("t :%-.5hhd\n", 136);
-// 	ft_printf("m :%-0.5hhd\n", 137);
-// 	printf("t :%-0.5hhd\n", 137);
-// 	ft_printf("m :%- .5hhd\n", 138);
-// 	printf("t :%- .5hhd\n", 138);
-// 	ft_printf("m :%-+.5hhd\n", 139);
-// 	printf("t :%-+.5hhd\n", 139);
-// 	ft_printf("m :%- 0.5hhd\n", 140);
-// 	printf("t :%- 0.5hhd\n", 140);
-// 	ft_printf("m :%-+0.5hhd\n", 141);
-// 	printf("t :%-+0.5hhd\n", 141);
-// 	ft_printf("m :%-+ 0.5hhd\n", 141);
-// 	printf("t :%-+ 0.5hhd\n", 141);
-// //_____convmin > convprecision
-// 	ft_printf("m :%-10.5hhd\n", 142);
-// 	printf("t :%-10.5hhd\n", 142);
-// 	ft_printf("m :%-010.5hhd\n", 143);
-// 	printf("t :%-010.5hhd\n", 143);
-// 	ft_printf("m :%- 10.5hhd\n", 144);
-// 	printf("t :%- 10.5hhd\n", 144);
-// 	ft_printf("m :%-+10.5hhd\n", 145);
-// 	printf("t :%-+10.5hhd\n", 145);
-// 	ft_printf("m :%- 010.5hhd\n", 146);
-// 	printf("t :%- 010.5hhd\n", 146);
-// 	ft_printf("m :%-+010.5hhd\n", 147);
-// 	printf("t :%-+010.5hhd\n", 147);
-// 	ft_printf("m :%-+ 010.5hhd\n", 147);
-// 	printf("t :%-+ 010.5hhd\n", 147);
+// // // // // ______convminwidth > len
+	// ft_printf("m :%-5hhd\n", 130);
+	// printf("t :%-5hhd\n", 130);
+	// ft_printf("m :%-05hhd\n", 131);
+	// printf("t :%-05hhd\n", 131);
+	// ft_printf("m :%- 5hhd\n", 132);
+	// printf("t :%- 5hhd\n", 132);
+	// ft_printf("m :%-+5hhd\n", 133);
+	// printf("t :%-+5hhd\n", 133);
+	// ft_printf("m :%- 05hhd\n", 134);
+	// printf("t :%- 05hhd\n", 134);
+	// ft_printf("m :%-+05hhd\n", 135);
+	// printf("t :%-+05hhd\n", 135);
+	// ft_printf("m :%-+ 05hhd\n", 135);
+	// printf("t :%-+ 05hhd\n", 135);
+// // // ________convprecision
+	// ft_printf("m :%-.5hhd\n", 136);
+	// printf("t :%-.5hhd\n", 136);
+	// ft_printf("m :%-0.5hhd\n", 137);
+	// printf("t :%-0.5hhd\n", 137);
+	// ft_printf("m :%- .5hhd\n", 138);
+	// printf("t :%- .5hhd\n", 138);
+	// ft_printf("m :%-+.5hhd\n", 139);
+	// printf("t :%-+.5hhd\n", 139);
+	// ft_printf("m :%- 0.5hhd\n", 140);
+	// printf("t :%- 0.5hhd\n", 140);
+	// ft_printf("m :%-+0.5hhd\n", 141);
+	// printf("t :%-+0.5hhd\n", 141);
+	// ft_printf("m :%-+ 0.5hhd\n", 141);
+	// printf("t :%-+ 0.5hhd\n", 141);
+// // //_____convmin > convprecision
+	// ft_printf("m :%-10.5hhd\n", 142);
+	// printf("t :%-10.5hhd\n", 142);
+	// ft_printf("m :%-010.5hhd\n", 143);
+	// printf("t :%-010.5hhd\n", 143);
+	// ft_printf("m :%- 10.5hhd\n", 144);
+	// printf("t :%- 10.5hhd\n", 144);
+	// ft_printf("m :%-+10.5hhd\n", 145);
+	// printf("t :%-+10.5hhd\n", 145);
+	// ft_printf("m :%- 010.5hhd\n", 146);
+	// printf("t :%- 010.5hhd\n", 146);
+	// ft_printf("m :%-+010.5hhd\n", 147);
+	// printf("t :%-+010.5hhd\n", 147);
+	// ft_printf("m :%-+ 010.5hhd\n", 147);
+	// printf("t :%-+ 010.5hhd\n", 147);
 // //______convminwidth < convprecision
-// 	ft_printf("m :%-5.10hhd\n", 148);
-// 	printf("t :%-5.10hhd\n", 148);
-// 	ft_printf("m :%-05.10hhd\n", 149);
-// 	printf("t :%-05.10hhd\n", 149);
-// 	ft_printf("m :%- 5.10hhd\n", 150);
-// 	printf("t :%- 5.10hhd\n", 150);
-// 	ft_printf("m :%-+5.10hhd\n", 151);
-// 	printf("t :%-+5.10hhd\n", 151);
-// 	ft_printf("m :%- 05.10hhd\n", 152);
-// 	printf("t :%- 05.10hhd\n", 152);
-// 	ft_printf("m :%-+05.10hhd\n", 153);
-// 	printf("t :%-+05.10hhd\n", 153);
-// 	ft_printf("m :%-+ 05.10hhd\n", 154);
-// 	printf("t :%-+ 05.10hhd\n", 154);
+	// ft_printf("m :%-5.10hhd\n", 148);
+	// printf("t :%-5.10hhd\n", 148);
+	// ft_printf("m :%-05.10hhd\n", 149);
+	// printf("t :%-05.10hhd\n", 149);
+	// ft_printf("m :%- 5.10hhd\n", 150);
+	// printf("t :%- 5.10hhd\n", 150);
+	// ft_printf("m :%-+5.10hhd\n", 151);
+	// printf("t :%-+5.10hhd\n", 151);
+	// ft_printf("m :%- 05.10hhd\n", 152);
+	// printf("t :%- 05.10hhd\n", 152);
+	// ft_printf("m :%-+05.10hhd\n", 153);
+	// printf("t :%-+05.10hhd\n", 153);
+	// ft_printf("m :%-+ 05.10hhd\n", 154);
+	// printf("t :%-+ 05.10hhd\n", 154);
 // //_____convminwidth == convprecision
-// 	ft_printf("m :%-5.5hhd\n", 154);
-// 	printf("t :%-5.5hhd\n", 154);
-// 	ft_printf("m :%-05.5hhd\n", 155);
-// 	printf("t :%-05.5hhd\n", 155);
-// 	ft_printf("m :%- 5.5hhd\n", 156);
-// 	printf("t :%- 5.5hhd\n", 156);
-// 	ft_printf("m :%-+5.5hhd\n", 157);
-// 	printf("t :%-+5.5hhd\n", 157);
-// 	ft_printf("m :%- 05.5hhd\n", 158);
-// 	printf("t :%- 05.5hhd\n", 158);
-// 	ft_printf("m :%-+05.5hhd\n", 159);
-// 	printf("t :%-+05.5hhd\n", 159);
-// 	ft_printf("m :%-+ 05.5hhd\n", 159);
-// 	printf("t :%-+ 05.5hhd\n", 159);
+	// ft_printf("m :%-5.5hhd\n", 154);
+	// printf("t :%-5.5hhd\n", 154);
+	// ft_printf("m :%-05.5hhd\n", 155);
+	// printf("t :%-05.5hhd\n", 155);
+	// ft_printf("m :%- 5.5hhd\n", 156);
+	// printf("t :%- 5.5hhd\n", 156);
+	// ft_printf("m :%-+5.5hhd\n", 157);
+	// printf("t :%-+5.5hhd\n", 157);
+	// ft_printf("m :%- 05.5hhd\n", 158);
+	// printf("t :%- 05.5hhd\n", 158);
+	// ft_printf("m :%-+05.5hhd\n", 159);
+	// printf("t :%-+05.5hhd\n", 159);
+	// ft_printf("m :%-+ 05.5hhd\n", 159);
+	// printf("t :%-+ 05.5hhd\n", 159);
 
 //TEST AVEC %hhd SANS MINUS
 
 //_____convminwidth < len 
-// 	ft_printf("m :%hhd\n", 123);
-// 	printf("t :%hhd\n", 123);
-// 	ft_printf("m :%1hhd\n", 124);
-// 	printf("t :%1hhd\n", 124);
-// 	ft_printf("m :%01hhd\n", 125);
-// 	printf("t :%01hhd\n", 125);
-// 	ft_printf("m :% 1hhd\n", 126);
-// 	printf("t :% 1hhd\n", 126);
-// 	ft_printf("m :%+1hhd\n", 127);
-// 	printf("t :%+1hhd\n", 127);
-// 	ft_printf("m :% 01hhd\n", 128);
-// 	printf("t :% 01hhd\n", 128);
-// 	ft_printf("m :%+01hhd\n", 129);
-// 	printf("t :%+01hhd\n", 129);
-// 	ft_printf("m :%+ 01hhd\n", 129);
-// 	printf("t :%+ 01hhd\n", 129);
+	// ft_printf("m :%hhd\n", 123);
+	// printf("t :%hhd\n", 123);
+	// ft_printf("m :%1hhd\n", 124);
+	// printf("t :%1hhd\n", 124);
+	// ft_printf("m :%01hhd\n", 125);
+	// printf("t :%01hhd\n", 125);
+	// ft_printf("m :% 1hhd\n", 126);
+	// printf("t :% 1hhd\n", 126);
+	// ft_printf("m :%+1hhd\n", 127);
+	// printf("t :%+1hhd\n", 127);
+	// ft_printf("m :% 01hhd\n", 128);
+	// printf("t :% 01hhd\n", 128);
+	// ft_printf("m :%+01hhd\n", 129);
+	// printf("t :%+01hhd\n", 129);
+	// ft_printf("m :%+ 01hhd\n", 129);
+	// printf("t :%+ 01hhd\n", 129);
 // //______convminwidth > len
-// 	ft_printf("m :%5hhd\n", 130);
-// 	printf("t :%5hhd\n", 130);
-// 	ft_printf("m :%05hhd\n", 131);
-// 	printf("t :%05hhd\n", 131);
-// 	ft_printf("m :% 5hhd\n", 132);
-// 	printf("t :% 5hhd\n", 132);
-// 	ft_printf("m :%+5hhd\n", 133);
-// 	printf("t :%+5hhd\n", 133);
-// 	ft_printf("m :% 05hhd\n", 134);
-// 	printf("t :% 05hhd\n", 134);
-// 	ft_printf("m :%+05hhd\n", 135);
-// 	printf("t :%+05hhd\n", 135);
-// 	ft_printf("m :%+ 05hhd\n", 135);
-// 	printf("t :%+ 05hhd\n", 135);
+	// ft_printf("m :%5hhd\n", 130);
+	// printf("t :%5hhd\n", 130);
+	// ft_printf("m :%05hhd\n", 131);
+	// printf("t :%05hhd\n", 131);
+	// ft_printf("m :% 5hhd\n", 132);
+	// printf("t :% 5hhd\n", 132);
+	// ft_printf("m :%+5hhd\n", 133);
+	// printf("t :%+5hhd\n", 133);
+	// ft_printf("m :% 05hhd\n", 134);
+	// printf("t :% 05hhd\n", 134);
+	// ft_printf("m :%+05hhd\n", 135);
+	// printf("t :%+05hhd\n", 135);
+	// ft_printf("m :%+ 05hhd\n", 135);
+	// printf("t :%+ 05hhd\n", 135);
 // //________convprecision
-// 	ft_printf("m :%.5hhd\n", 136);
-// 	printf("t :%.5hhd\n", 136);
-// 	ft_printf("m :%0.5hhd\n", 137);
-// 	printf("t :%0.5hhd\n", 137);
-// 	ft_printf("m :% .5hhd\n", 138);
-// 	printf("t :% .5hhd\n", 138);
-// 	ft_printf("m :%+.5hhd\n", 139);
-// 	printf("t :%+.5hhd\n", 139);
-// 	ft_printf("m :% 0.5hhd\n", 140);
-// 	printf("t :% 0.5hhd\n", 140);
-// 	ft_printf("m :%+0.5hhd\n", 141);
-// 	printf("t :%+0.5hhd\n", 141);
-// 	ft_printf("m :%+ 0.5hhd\n", 141);
-// 	printf("t :%+ 0.5hhd\n", 141);
+	// ft_printf("m :%.5hhd\n", 136);
+	// printf("t :%.5hhd\n", 136);
+	// ft_printf("m :%0.5hhd\n", 137);
+	// printf("t :%0.5hhd\n", 137);
+	// ft_printf("m :% .5hhd\n", 138);
+	// printf("t :% .5hhd\n", 138);
+	// ft_printf("m :%+.5hhd\n", 139);
+	// printf("t :%+.5hhd\n", 139);
+	// ft_printf("m :% 0.5hhd\n", 140);
+	// printf("t :% 0.5hhd\n", 140);
+	// ft_printf("m :%+0.5hhd\n", 141);
+	// printf("t :%+0.5hhd\n", 141);
+	// ft_printf("m :%+ 0.5hhd\n", 141);
+	// printf("t :%+ 0.5hhd\n", 141);
 // //_____convmin > convprecision
-// 	ft_printf("m :%10.5hhd\n", 142);
-// 	printf("t :%10.5hhd\n", 142);
-// 	ft_printf("m :%010.5hhd\n", 143);
-// 	printf("t :%010.5hhd\n", 143);
-// 	ft_printf("m :% 10.5hhd\n", 144);
-// 	printf("t :% 10.5hhd\n", 144);
-// 	ft_printf("m :%+10.5hhd\n", 145);
-// 	printf("t :%+10.5hhd\n", 145);
-// 	ft_printf("m :% 010.5hhd\n", 146);
-// 	printf("t :% 010.5hhd\n", 146);
-// 	ft_printf("m :%+010.5hhd\n", 147);
-// 	printf("t :%+010.5hhd\n", 147);
-// 	ft_printf("m :%+ 010.5hhd\n", 147);
-// 	printf("t :%+ 010.5hhd\n", 147);
+	// ft_printf("m :%10.5hhd\n", 115);
+	// printf("t :%10.5hhd\n", 115);
+	// ft_printf("m :%010.5hhd\n", 143);
+	// printf("t :%010.5hhd\n", 143);
+	// ft_printf("m :% 10.5hhd\n", 144);
+	// printf("t :% 10.5hhd\n", 144);
+	// ft_printf("m :%+10.5hhd\n", 145);
+	// printf("t :%+10.5hhd\n", 145);
+	// ft_printf("m :% 010.5hhd\n", 146);
+	// printf("t :% 010.5hhd\n", 146);
+	// ft_printf("m :%+010.5hhd\n", 147);
+	// printf("t :%+010.5hhd\n", 147);
+	// ft_printf("m :%+ 010.5hhd\n", 147);
+	// printf("t :%+ 010.5hhd\n", 147);
 // //______convminwidth < convprecision
-// 	ft_printf("m :%5.10hhd\n", 148);
-// 	printf("t :%5.10hhd\n", 148);
-// 	ft_printf("m :%05.10hhd\n", 149);
-// 	printf("t :%05.10hhd\n", 149);
-// 	ft_printf("m :% 5.10hhd\n", 150);
-// 	printf("t :% 5.10hhd\n", 150);
-// 	ft_printf("m :%+5.10hhd\n", 151);
-// 	printf("t :%+5.10hhd\n", 151);
-// 	ft_printf("m :% 05.10hhd\n", 152);
-// 	printf("t :% 05.10hhd\n", 152);
-// 	ft_printf("m :%+05.10hhd\n", 153);
-// 	printf("t :%+05.10hhd\n", 153);
-// 	ft_printf("m :%+ 05.10hhd\n", 154);
-// 	printf("t :%+ 05.10hhd\n", 154);
+	// ft_printf("m :%5.10hhd\n", 148);
+	// printf("t :%5.10hhd\n", 148);
+	// ft_printf("m :%05.10hhd\n", 149);
+	// printf("t :%05.10hhd\n", 149);
+	// ft_printf("m :% 5.10hhd\n", 150);
+	// printf("t :% 5.10hhd\n", 150);
+	// ft_printf("m :%+5.10hhd\n", 151);
+	// printf("t :%+5.10hhd\n", 151);
+	// ft_printf("m :% 05.10hhd\n", 152);
+	// printf("t :% 05.10hhd\n", 152);
+	// ft_printf("m :%+05.10hhd\n", 153);
+	// printf("t :%+05.10hhd\n", 153);
+	// ft_printf("m :%+ 05.10hhd\n", 154);
+	// printf("t :%+ 05.10hhd\n", 154);
 // //_____convminwidth == convprecision
-// 	ft_printf("m :%5.5hhd\n", 154);
-// 	printf("t :%5.5hhd\n", 154);
-// 	ft_printf("m :%05.5hhd\n", 155);
-// 	printf("t :%05.5hhd\n", 155);
-// 	ft_printf("m :% 5.5hhd\n", 156);
-// 	printf("t :% 5.5hhd\n", 156);
-// 	ft_printf("m :%+5.5hhd\n", 157);
-// 	printf("t :%+5.5hhd\n", 157);
-// 	ft_printf("m :% 05.5hhd\n", 158);
-// 	printf("t :% 05.5hhd\n", 158);
-// 	ft_printf("m :%+05.5hhd\n", 159);
-// 	printf("t :%+05.5hhd\n", 159);
-// 	ft_printf("m :%+ 05.5hhd\n", 159);
-// 	printf("t :%+ 05.5hhd\n", 159);
+	ft_printf("m :%5.5hhd\n", 154);
+	printf("t :%5.5hhd\n", 154);
+	ft_printf("m :%05.5hhd\n", 155);
+	printf("t :%05.5hhd\n", 155);
+	ft_printf("m :% 5.5hhd\n", 156);
+	printf("t :% 5.5hhd\n", 156);
+	ft_printf("m :%+5.5hhd\n", 157);
+	printf("t :%+5.5hhd\n", 157);
+	ft_printf("m :% 05.5hhd\n", 158);
+	printf("t :% 05.5hhd\n", 158);
+	ft_printf("m :%+05.5hhd\n", 159);
+	printf("t :%+05.5hhd\n", 159);
+	ft_printf("m :%+ 05.5hhd\n", 159);
+	printf("t :%+ 05.5hhd\n", 159);
 
 // TEST AVEC %d SANS MINUS
 // _____convminwidth < len 
@@ -318,8 +318,8 @@ int		main(int ac, char **av)
 // 	ft_printf("m :%+ 01d\n", 129);
 // 	printf("t :%+ 01d\n", 129);
 // //______convminwidth > len
-// 	ft_printf("m :%5d\n", 130);
-// 	printf("t :%5d\n", 130);
+	// ft_printf("m :%5d\n", 130);
+	// printf("t :%5d\n", 130);
 // 	ft_printf("m :%05d\n", 131);
 // 	printf("t :%05d\n", 131);
 // 	ft_printf("m :% 5d\n", 132);
