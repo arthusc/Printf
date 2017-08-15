cd457ef33295788a22ae949966edfb6c4473d92b
Mikael BRIFFAUD <mbriffau@student.42.fr>
2017-08-15 02:31:27 +0200

    update conv_c ok

--- Printf/conv_c.c
+++ Printf/conv_c.c
@@ -6,13 +6,28 @@
 /*   By: mbriffau <mbriffau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/27 15:10:00 by mbriffau          #+#    #+#             */
-/*   Updated: 2017/08/11 14:52:52 by mbriffau         ###   ########.fr       */
+/*   Updated: 2017/08/15 02:30:02 by mbriffau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/ft_printf.h"
 
-//nb_bytes <= MB_CUR_MAX define in stdlib.h
+static int		count_wint(wint_t c)
+{
+	int		size;
+
+	size = 0;
+	if (c <= 0x7F)
+		size = 1;
+	else if (c <= 0x7FF)
+		size = 2;
+	else if (c <= 0xFFFF)
+		size = 3;
+	else if (c <= 0x10FFFF)
+		size = 4;
+	return (size);
+}
+
 void	print_wint(wint_t wint)
 {
 	char	str[4];
@@ -42,15 +57,57 @@ void	print_wint(wint_t wint)
 	}
 }
 
+static t_conv	*option_c(int print_size, char c, t_conv *conv, char print)
+{
+	int			i;
+	char		tab[(conv->min_width - print_size) + 1];
+	int 		size;
+
+	size = (conv->min_width - print_size);
+	i = 0;
+	tab[size] = '\0';
+	if (print)
+	{
+		conv->flag & MODIFIER_L ?
+		print_wint(c) : ft_putchar(print);
+	}
+	if (!size)
+		return (conv);
+	else
+	{
+		while (i < size)
+			tab[i++] = c;
+		write(1, tab, size);
+	}
+	conv->min_width = 0;
+	return (conv);
+}
+
+
 void	conv_c(t_printf *pf, t_conv *conv)
 {
-	(conv->flag & ZERO && !(conv->flag & MINUS)) ?
-	(conv = option_print(1, '0', conv, 0)) : 0;
-	conv->flag & SPACE ?
-	(conv = option_print(1, ' ', conv, 0)) : 0;
-	conv->flag & MODIFIER_L ? print_wint(va_arg(pf->ap, wint_t)) :
-	ft_putchar(va_arg(pf->ap, unsigned));
-	conv->flag & MINUS ?
-	(conv = option_print(1, ' ', conv, 0)) : 0;
+	int len;
+	wint_t c;
+
+	c = 0;
+	if (conv->flag & MODIFIER_L)
+	{	
+		c = va_arg(pf->ap, wint_t);
+		len = count_wint(c);
+		conv->flag & ZERO && !(conv->flag & MINUS) ? option_c(len, '0', &*conv, 0) : 0;
+		conv->flag & SPACE ? option_c(len, ' ', &*conv, 0) : 0;
+		conv->min_width > len && !(conv->flag & MINUS) ? option_c(len, ' ', &*conv, 0) : 0;
+		conv->flag & MINUS ? print_wint(c) : 0 ;
+		conv->flag & MINUS ? option_c(len, ' ', &*conv, 0) : print_wint(c);
+	}
+	else
+	{
+		c = va_arg(pf->ap, unsigned);
+		(conv->flag & ZERO && !(conv->flag & MINUS)) ?
+		option_c(1, '0', &*conv, 0) : 0;
+		conv->flag & SPACE ? option_c(1, ' ', &*conv, 0) : 0;
+		conv->min_width > 1 && !(conv->flag & MINUS) ? option_c(1, ' ', &*conv, 0) : 0;
+		conv->flag & MINUS ? option_c(1, ' ', &*conv, c) : ft_putchar(c);
+	}
 	return ;
 }
