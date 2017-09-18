984b874c35d76a9cf5d17dc1e65604b081456977
Mikael BRIFFAUD <mbriffau@student.42.fr>
2017-09-19 00:27:46 +0200

    move ft_error

--- srcs/ft_error_management.c
+++ srcs/ft_error_management.c
@@ -6,28 +6,23 @@
 /*   By: mbriffau <mbriffau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/22 00:52:04 by mbriffau          #+#    #+#             */
-/*   Updated: 2017/08/22 02:14:31 by mbriffau         ###   ########.fr       */
+/*   Updated: 2017/09/19 00:27:00 by mbriffau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_printf.h"
 
-void	ft_error_pf(char *file, char *func, int line, char *s)
+void			ft_error_pf(char *file, char *func, int line, char *s)
 {
 	ft_putstr("\033[1;37m----- \033[1;31mERROR\033[1;37m ----------\033[0m\n");
 	ft_putstr("\033[1;37mfile\033[0m		: ");
 	ft_putstr(file);
-	ft_putstr("\n\033[1;37mfunction\033[0m	: "); 
+	ft_putstr("\n\033[1;37mfunction\033[0m	: ");
 	ft_putstr(func);
-	ft_putstr("\n\033[1;37mline\033[0m		: ("); 
+	ft_putstr("\n\033[1;37mline\033[0m		: (");
 	ft_putnbr(line);
 	ft_putstr(")\n\033[1;37merror message\033[0m	: ");
 	ft_putstr(s);
 	ft_putstr("\n\033[1;37m----------------------\033[0m");
 	exit(-1);
 }
-
-// void	error_usage(char *s)
-// {
-		
-// }
--- srcs/ft_printf.c
+++ srcs/ft_printf.c
@@ -6,26 +6,11 @@
 /*   By: mbriffau <mbriffau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/27 15:41:18 by mbriffau          #+#    #+#             */
-/*   Updated: 2017/09/19 00:23:54 by mbriffau         ###   ########.fr       */
+/*   Updated: 2017/09/19 00:26:59 by mbriffau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_printf.h"
-
-void			ft_error_pf(char *file, char *func, int line, char *s)
-{
-	ft_putstr("\033[1;37m----- \033[1;31mERROR\033[1;37m ----------\033[0m\n");
-	ft_putstr("\033[1;37mfile\033[0m		: ");
-	ft_putstr(file);
-	ft_putstr("\n\033[1;37mfunction\033[0m	: ");
-	ft_putstr(func);
-	ft_putstr("\n\033[1;37mline\033[0m		: (");
-	ft_putnbr(line);
-	ft_putstr(")\n\033[1;37merror message\033[0m	: ");
-	ft_putstr(s);
-	ft_putstr("\n\033[1;37m----------------------\033[0m");
-	exit(-1);
-}
 
 static void		print_buffer(char *s, int size)
 {
