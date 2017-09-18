334cfeb9709a2504e488e6c8f4c9bd832b8e8dee
Mikael BRIFFAUD <mbriffau@student.42.fr>
2017-09-19 00:24:32 +0200

    ft_printf.c norme ok

--- srcs/ft_printf.c
+++ srcs/ft_printf.c
@@ -6,20 +6,20 @@
 /*   By: mbriffau <mbriffau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/27 15:41:18 by mbriffau          #+#    #+#             */
-/*   Updated: 2017/09/18 22:37:01 by mbriffau         ###   ########.fr       */
+/*   Updated: 2017/09/19 00:23:54 by mbriffau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_printf.h"
 
-void	ft_error_pf(char *file, char *func, int line, char *s)
+void			ft_error_pf(char *file, char *func, int line, char *s)
 {
 	ft_putstr("\033[1;37m----- \033[1;31mERROR\033[1;37m ----------\033[0m\n");
 	ft_putstr("\033[1;37mfile\033[0m		: ");
 	ft_putstr(file);
-	ft_putstr("\n\033[1;37mfunction\033[0m	: "); 
+	ft_putstr("\n\033[1;37mfunction\033[0m	: ");
 	ft_putstr(func);
-	ft_putstr("\n\033[1;37mline\033[0m		: ("); 
+	ft_putstr("\n\033[1;37mline\033[0m		: (");
 	ft_putnbr(line);
 	ft_putstr(")\n\033[1;37merror message\033[0m	: ");
 	ft_putstr(s);
@@ -27,14 +27,16 @@ void	ft_error_pf(char *file, char *func, int line, char *s)
 	exit(-1);
 }
 
-static void	print_buffer(char *s, int size)
+static void		print_buffer(char *s, int size)
 {
 	write(1, s, size);
 }
 
 static t_printf	*print_until(t_printf *pf, char *format, int i, int c)
 {
-	int n = 0;
+	int		n;
+
+	n = 0;
 	while (format[i + n] != '\0' && format[i + n] != c)
 		n++;
 	buffer(&*pf, &format[pf->i], n);
@@ -42,14 +44,12 @@ static t_printf	*print_until(t_printf *pf, char *format, int i, int c)
 	return (pf);
 }
 
-
-int		ft_printf(char *format, ...)
+int				ft_printf(char *format, ...)
 {
 	t_printf	pf;
 	char		tab[BUFFER_SIZE];
 
 	ft_bzero(&pf, sizeof(pf));
-
 	ft_bzero(&tab[0], BUFFER_SIZE);
 	va_start(pf.ap, format);
 	pf.format = format;
@@ -66,15 +66,14 @@ int		ft_printf(char *format, ...)
 		if (pf.format[pf.i])
 			print_until(&pf, pf.format, pf.i, '%');
 	}
-	pf.buffer &&  pf.i_buf? print_buffer(pf.buffer, pf.i_buf) : 0;
+	pf.buffer && pf.i_buf ? print_buffer(pf.buffer, pf.i_buf) : 0;
 	va_end(pf.ap);
 	return (pf.i_buf - pf.subtract_buffer);
 }
 
-t_printf	*buffer(t_printf *pf, char *saved, int len)
+t_printf		*buffer(t_printf *pf, char *saved, int len)
 {
-	int i;
-	static int a = 0;
+	int			i;
 
 	i = 0;
 	while (saved && (len - i) && (pf->i_buf < BUFFER_SIZE))
@@ -83,7 +82,6 @@ t_printf	*buffer(t_printf *pf, char *saved, int len)
 		pf->i_buf++;
 		i++;
 	}
-	a++;
 	pf->buffer[pf->i_buf] = '\0';
 	return (pf);
 }
