e53b58ea94f0d03b35ec834ac5ea83132dce0474
Arthus CHAMBON <achambon@e3r2p18.42.fr>
2017-09-28 21:19:10 +0200

    .h

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,11 +6,12 @@
 /*   By: achambon <achambon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/17 18:34:23 by mbriffau          #+#    #+#             */
-/*   Updated: 2017/09/28 16:57:27 by achambon         ###   ########.fr       */
+/*   Updated: 2017/09/28 21:18:21 by achambon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
 # include "../libft/srcs/libft.h"
 # include <stdarg.h>
 # include <errno.h>
@@ -19,16 +20,13 @@
 # include <sys/types.h>
 # include <sys/stat.h>
 # include <fcntl.h>
-# include "macros_pf.h"
-
-# include <stdio.h>// to delete
 
 typedef struct			s_conv
 {
 	int					min_width;
 	int 				width_temp;
 	int					precision;
-	int					precision_set;//////////
+	int					precision_set;
 	int					ox;
 	int					before;
 	char				modif;
@@ -50,83 +48,64 @@ typedef struct			s_printf
 	int					add_buffer;
 }						t_printf;
 
-void		ft_error_pf(char *file, char *func, int line, char *s);
-
-int			ft_printf(char *format, ...);
-t_printf	*parse_conversion(t_printf *pf);
-int			parse_wildcard(t_printf *pf, t_conv *conv);
-t_conv		*init_conv(void);
-t_printf	*print_wint(t_printf *pf, wint_t wint);
-// void		print_p(t_printf *pf, int len, char *str);
-
-t_printf	parsing_color(t_printf *pf);
-// int			option(t_printf *pf, int n, char c, t_conv *conv, char *s);
-// t_conv		*option_print(int print_size, char c, t_conv *conv, char *s);
-t_printf	*buffer(t_printf *pf, char *saved, int len);
-
-t_printf	*conversion_specifier(t_printf *pf, t_conv *conv);
-
+void					ft_error_pf(char *file, char *func, int line, char *s);
+int						ft_printf(char *format, ...);
+int						parse_type(char c);
+int						parse_wildcard(t_printf *pf, t_conv *conv);
+t_conv					*init_conv(void);
+t_printf				*print_wint(t_printf *pf, wint_t wint);
+t_printf				parsing_color(t_printf *pf);
+t_printf				*buffer(t_printf *pf, char *saved, int len);
+t_printf				*conversion_specifier(t_printf *pf, t_conv *conv);
+t_printf				*parse_conversion(t_printf *pf);
 /*
 **conv_c
 */
-void		conv_c(t_printf *pf, t_conv *conv);
-t_conv		*option_char(t_printf *pf, int print_size, char c, t_conv *conv);
-
+void					conv_c(t_printf *pf, t_conv *conv);
+t_conv					*option_char(t_printf *pf, int print_size, char c, t_conv *conv);
 /*
 **conv_s
 */
-void		conv_s(t_printf *pf, t_conv *conv);
-void		conv_s_file(t_printf *pf);
+void					conv_s(t_printf *pf, t_conv *conv);
+void					conv_s_file(t_printf *pf);
 /*
 **conv_d
 */
-void		conv_d(t_printf *pf, t_conv *conv);
-int			conv_d_prec_only(t_printf *pf, t_conv *conv, int len, char *str);
-int			conv_d_minus(t_printf *pf, t_conv *conv, int width_temp, char *str);
-int			conv_d_width_only(t_printf *pf, t_conv *conv, int width_temp, char *str);
-int			conv_d_prec_only(t_printf *pf, t_conv *conv, int len, char *str);
-// int			conv_d_minus2(t_printf *pf, t_conv *conv, int len, char *str);
-int			conv_d_hh_nominus(t_printf *pf, t_conv *conv, char *str, int len);
-int		option_d(t_printf *pf, int n, char c, t_conv *conv);
-char		*ft_itoa_printf(long long n);
+void					conv_d(t_printf *pf, t_conv *conv);
+int						conv_d_prec_only(t_printf *pf, t_conv *conv, int len, char *str);
+int						conv_d_minus(t_printf *pf, t_conv *conv, int width_temp, char *str);
+int						conv_d_width_only(t_printf *pf, t_conv *conv, int width_temp, char *str);
+int						conv_d_prec_only(t_printf *pf, t_conv *conv, int len, char *str);
+int						conv_d_hh_nominus(t_printf *pf, t_conv *conv, char *str, int len);
+int						option_d(t_printf *pf, int n, char c, t_conv *conv);
+char					*ft_itoa_printf(long long n);
 /*
 **conv_x
 */
-void		conv_x(t_printf *pf, t_conv *conv);
-int			conv_x_minus(t_printf *pf, t_conv *conv, char *str, int len);
-int			conv_x_width_only(t_printf *pf, t_conv *conv, int len, char *str);
-t_conv		option_x(t_printf *pf, int n, char c, t_conv *conv);
-t_printf	*add_0x(t_printf *pf, t_conv *conv);
-char		*ft_str_tolower(char *s);
-
+void					conv_x(t_printf *pf, t_conv *conv);
+int						conv_x_minus(t_printf *pf, t_conv *conv, char *str, int len);
+int						conv_x_width_only(t_printf *pf, t_conv *conv, int len, char *str);
+t_conv					option_x(t_printf *pf, int n, char c, t_conv *conv);
+t_printf				*add_0x(t_printf *pf, t_conv *conv);
+char					*ft_str_tolower(char *s);
 /*
 **number.c
 */
-intmax_t	ptr_number_base(t_printf *pf, int f);
-uintmax_t	ptr_number(t_printf *pf, int f);
-char		*ft_uitoa_pf(unsigned long n);
+t_conv					option_p(t_printf *pf, int n, char c, t_conv *conv);
+intmax_t				ptr_number_base(t_printf *pf, int f);
+uintmax_t				ptr_number(t_printf *pf, int f);
+char					*ft_uitoa_pf(unsigned long n);
+int						option(t_printf *pf, int c, size_t n);
+int						conv_p(t_printf *pf, t_conv *conv);
+int						print_p(t_printf *pf, t_conv *conv, int len, char *str);
+int						conv_p_minus(t_printf *pf, t_conv *conv, int len, char *str);
+int						add_char_and_string_2_buff(t_printf *pf, char c, char *str, int len);
+int						minwidth_decr_add_char_2_buff(t_printf *pf, char c, t_conv *conv);
+int						special_hhd_reverse_0_n_minus(t_printf *pf, char *str, char c);
+void					conv_b(t_printf *pf, t_conv *conv);
+void					conv_percent(t_printf *pf, t_conv *conv);
+void					no_conv(t_printf *pf, t_conv *conv);
+void					conv_f(t_printf *pf, t_conv *conv);
+void					conv_f_L(t_printf *pf, t_conv *conv);
 
-
-
-
-int			option(t_printf *pf, int c, size_t n);
-
-void		conv_f(t_printf *pf, t_conv *conv);
-void		conv_f_L(t_printf *pf, t_conv *conv);
-
-
-int			conv_p(t_printf *pf, t_conv *conv);
-t_conv	option_p(t_printf *pf, int n, char c, t_conv *conv);
-int		print_p(t_printf *pf, t_conv *conv, int len, char *str);
-int		conv_p_minus(t_printf *pf, t_conv *conv, int len, char *str);
-
-
-
-void		conv_b(t_printf *pf, t_conv *conv);
-void		conv_percent(t_printf *pf, t_conv *conv);
-void		no_conv(t_printf *pf, t_conv *conv);
-
-int			add_char_and_string_2_buff(t_printf *pf, char c, char *str, int len);
-int			minwidth_decr_add_char_2_buff(t_printf *pf, char c, t_conv *conv);
-int			special_hhd_reverse_0_n_minus(t_printf *pf, char *str, char c);
-// int			if_tick_but_no_prec(t_printf *pf, t_conv *conv);
+#endif
